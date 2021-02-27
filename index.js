9c0c049ca5d2591e88de185867d1ee71c44e9033
skk7414 <skk7414@naver.com>
2021-02-27 18:20:13 +0900

    bot upload

--- index.js
+++ index.js
@@ -246,39 +246,4 @@ module.exports = {
       message.channel.send(avatarList);
   },
 };
-const { MessageEmbed } = require("discord.js")
-const moment = require("moment")
-
-module.exports = {
-  name: "아바타",
-  aliases: ["whois", "user"],
-  usage: "userinfo <MENTION>",
-  description: "Get advance stats of given person or yourself",
-  run: async (client, message, args) => {
-
-
-
-    //OPTIONS FOR STATUS
-const embed = new MessageEmbed()
-            .setColor('BLUE')
-            .setTitle(`${message.author.username}님의 프사`)
-            .setImage(`${message.author.displayAvatarURL({ dynamic: true })}`)
-
-            
-            
-
-            
-
-
-        
-    
-            
-            
-    
-            
-            
-            message.channel.send(embed);
-        }
-    }
-    
 client.login(token)
